be67ac50c1109246c937e374862feb2e94c5ad98
AW-2021 <aminawasif20@gmail.com>
2024-07-24 13:27:07 +0500

    Extended script.js file

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -10,8 +10,9 @@ let firstName = "Matilda";
 
 console.log(firstName);
 console.log(firstName);
-console.log(firstName);
+//console.log(firstName);
 
+// Variable name conventions
 let jonas_matilda = "JM";
 let $function = 27;
 
@@ -25,3 +26,72 @@ let job1 = 'programmer';
 let job2 = 'teacher';
 
 console.log(myFirstJob);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1995;
+console.log(typeof year);
+console.log(typeof null); // -> object (null is not an object, legacy reasons for this bug)
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990; (NOT ALLOWED) 
+// const job; (NOT ALLOWED, Must be initialized)
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Henry';
+console.log(lastName);
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 -> 3 to the power of 2 = 2 * 2 * 2 = 8
+
+const firstName1 = 'Jonas';
+const lastName1 = 'Henry';
+console.log(firstName1 + ' ' + lastName1);
+
+// Assignment operators
+let x = 10 + 5;
+x += 10; // x = x + 10
+x *= 4; // x = x * 10
+x++; // x = x + 1
+x--; // x = x - 1
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+
+console.log(ageSarah >= 18);
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+let y, z;
+y = z = 25 - 10 - 5;
+console.log(y, z);
+
+const averageAge = (ageSarah + ageJonas) / 2;
+console.log(ageJonas, ageSarah, averageAge);
