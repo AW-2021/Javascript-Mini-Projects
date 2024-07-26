78812545a459c4027597c01a10f4dd6d2afc41a0
AW-2021 <aminawasif20@gmail.com>
2024-07-26 15:15:22 +0500

    Extended script.js file further

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -183,3 +183,51 @@ if (friendsArr.includes('Steven')) {
 
 /**********************************************************************************************************************/
 
+// OBJECTS
+
+const jonasArray = [
+    'Jonas',
+    'Henry',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+];
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Henry',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+}
+
+/**********************************************************************************************************************/
+
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+const interestedIn = prompt(`What do you want to know about Jonas? 
+Choose between firstName, lastName, age, job or friends.`);
+
+if (jonas[interestedIn]) {
+    console.log(jonas[interestedIn]);
+} else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job or friends.');
+}
+
+jonas.location = 'Denmark';
+jonas['twitter handle'] = '@jonashenry';
+console.log(jonas);
+
+// CHALLENGE
+// Dynamically write the sentence: Jonas has 3 friends, and his best friend is called Michael
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
+
+/**********************************************************************************************************************/
+
