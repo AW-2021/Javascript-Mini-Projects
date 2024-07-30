5abf31532238aeac12f9ea79e6bb29626c801d0a
AW-2021 <aminawasif20@gmail.com>
2024-07-30 16:49:25 +0500

    Modified script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+/*console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 console.log(document.querySelector('.message').textContent);
 
@@ -8,4 +8,13 @@ document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 20;
 
 document.querySelector('.guess').value = 8;
-console.log(document.querySelector('.guess').value);
+console.log(document.querySelector('.guess').value);*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '❌ No number!';
+  }
+});
