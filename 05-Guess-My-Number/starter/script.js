0d590d606fa7b7760ded98efced0ec5a3b7cb852
AW-2021 <aminawasif20@gmail.com>
2024-07-30 17:33:30 +0500

    Modified script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,11 +10,26 @@ document.querySelector('.score').textContent = 20;
 document.querySelector('.guess').value = 8;
 console.log(document.querySelector('.guess').value);*/
 
+const secretNumber = Math.floor(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+// console.log(secretNumber);
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(typeof guess);
+  console.log(guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '❌ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Guess too high!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Guess too low!';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
