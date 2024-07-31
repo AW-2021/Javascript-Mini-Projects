b337805479dcaeb22a4eac757a3af6d4df78b861
AW-2021 <aminawasif20@gmail.com>
2024-07-31 13:22:20 +0500

    Refactoring of code in script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,15 +1,5 @@
 'use strict';
 
-/*console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
-
-document.querySelector('.guess').value = 8;
-console.log(document.querySelector('.guess').value);*/
-
 let secretNumber = Math.floor(Math.random() * 20) + 1; // Can use Math.trunc as well
 let score = 20;
 let highScore = 0;
@@ -19,49 +9,38 @@ const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
 };
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+const correctGuess = function () {
+  displayMessage('🎉 Correct Number!');
+  document.querySelector('.number').textContent = secretNumber;
 
-  // When there is no input
-  if (!guess) {
-    displayMessage('❌ No number!');
+  document.querySelector('body').style.backgroundColor = '#60b347';
+  document.querySelector('.number').style.width = '30rem';
+  document.querySelector('.number').style.backgroundColor = 'white';
+
+  // Setting high score
+  highScore = score > highScore ? score : highScore;
+  document.querySelector('.highscore').textContent = highScore;
+};
+
+const wrongGuess = function (guess) {
+  displayMessage(
+    guess > secretNumber ? '📈 Guess too high!' : '📉 Guess too low!'
+  );
+  if (score > 1) {
+    score--;
+    document.querySelector('.score').textContent = score;
   }
-  // When player wins
-  else if (guess === secretNumber) {
-    displayMessage('🎉 Correct Number!');
-    document.querySelector('.number').textContent = secretNumber;
+  // When player loses
+  else {
+    displayMessage('☠️ You lost the game!');
+    document.querySelector('.score').textContent = 0;
 
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.number').style.backgroundColor = 'white';
-
-    // Setting high score
-    highScore = score > highScore ? score : highScore;
-    document.querySelector('.highscore').textContent = highScore;
+    document.querySelector('body').style.backgroundColor = 'maroon';
+    document.querySelector('.number').style.backgroundColor = 'darkgrey';
   }
-  // When guess is wrong
-  else if (guess !== secretNumber) {
-    displayMessage(
-      guess > secretNumber ? '📈 Guess too high!' : '📉 Guess too low!'
-    );
-    if (score > 1) {
-      score--;
-      document.querySelector('.score').textContent = score;
-    }
-    // When player loses
-    else {
-      displayMessage('☠️ You lost the game!');
-      document.querySelector('.score').textContent = 0;
+};
 
-      document.querySelector('body').style.backgroundColor = 'maroon';
-      document.querySelector('.number').style.backgroundColor = 'darkgrey';
-    }
-  }
-});
-
-// Game reset functionality
-document.querySelector('.again').addEventListener('click', function () {
+const reset = function () {
   // Initial values of the score & secretNumber restored
   score = 20;
   secretNumber = Math.floor(Math.random() * 20) + 1;
@@ -75,4 +54,27 @@ document.querySelector('.again').addEventListener('click', function () {
   // Original body background color & number width restored
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When there is no input
+  if (!guess) {
+    displayMessage('❌ No number!');
+  }
+  // When player wins
+  else if (guess === secretNumber) {
+    correctGuess(guess);
+  }
+  // When guess is wrong
+  else if (guess !== secretNumber) {
+    wrongGuess(guess);
+  }
+});
+
+// Game reset functionality
+document.querySelector('.again').addEventListener('click', function () {
+  reset();
 });
