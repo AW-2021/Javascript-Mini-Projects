55d5deec67fdb445b2300fc1d2fbded77abcc5dc
AW-2021 <aminawasif20@gmail.com>
2024-07-31 11:22:30 +0500

    Added reset functionality to script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,19 +10,22 @@ document.querySelector('.score').textContent = 20;
 document.querySelector('.guess').value = 8;
 console.log(document.querySelector('.guess').value);*/
 
-const secretNumber = Math.floor(Math.random() * 20) + 1; // Can use Math.trunc as well
+let secretNumber = Math.floor(Math.random() * 20) + 1; // Can use Math.trunc as well
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
-// console.log(secretNumber);
+console.log(secretNumber);
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '❌ No number!';
-  } else if (guess === secretNumber) {
+  }
+  // When player wins
+  else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -33,7 +36,9 @@ document.querySelector('.check').addEventListener('click', function () {
     if (score > 1) {
       score--;
       document.querySelector('.score').textContent = score;
-    } else {
+    }
+    //When player loses
+    else {
       document.querySelector('.message').textContent = '☠️ You lost the game!';
       document.querySelector('.score').textContent = 0;
 
@@ -41,4 +46,21 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.number').style.backgroundColor = 'darkgrey';
     }
   }
+});
+
+// Game reset functionality
+document.querySelector('.again').addEventListener('click', function () {
+  // Initial values of the score & secretNumber restored
+  score = 20;
+  secretNumber = Math.floor(Math.random() * 20) + 1;
+
+  // Restore initial conditions of message, number, score & guess input field
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+
+  // Original body background color & number width restored
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
