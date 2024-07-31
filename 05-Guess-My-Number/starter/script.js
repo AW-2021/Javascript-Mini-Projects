66a0f8b952dc66d8cba64b9ef69a114d51decfa1
AW-2021 <aminawasif20@gmail.com>
2024-07-31 10:21:15 +0500

    Improved check and score functionality in script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -23,18 +23,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '❌ No number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
-  } else if (guess > secretNumber) {
+  } else if (guess > secretNumber || guess < secretNumber) {
+    document.querySelector('.message').textContent =
+      guess > secretNumber ? '📈 Guess too high!' : '📉 Guess too low!';
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Guess too high!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '☠️ You lost the game!';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Guess too low!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
