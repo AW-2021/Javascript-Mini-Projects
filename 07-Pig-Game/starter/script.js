9a9ce8f1f351190df9c1f11981228dde7b18b880
AW-2021 <aminawasif20@gmail.com>
2024-08-01 14:58:47 +0500

    Added Roll Dice functionality

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -15,3 +15,29 @@ console.log(currentPlayer);
 let currentScore = 0;
 let totalScore = 0;
 let random;
+
+const rollDice = function () {
+  random = Math.floor(Math.random() * 6) + 1;
+  console.log(random);
+  dice.setAttribute('src', `dice-${random}.png`);
+
+  if (random === 1) {
+    player1.classList.toggle('player--active');
+    player2.classList.toggle('player--active');
+
+    if (currentPlayer === player1) {
+      currentPlayer = player2;
+    } else {
+      currentPlayer = player1;
+    }
+    currentScore = Number(
+      currentPlayer.querySelector('.current-score').textContent
+    );
+    totalScore = Number(currentPlayer.querySelector('.score').textContent);
+  } else {
+    currentScore += random;
+    currentPlayer.querySelector('.current-score').textContent = currentScore;
+  }
+};
+
+rollBtn.addEventListener('click', rollDice);
