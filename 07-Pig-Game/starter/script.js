7b9a8ff1b311479708f2eaefd909b5043015762e
AW-2021 <aminawasif20@gmail.com>
2024-08-01 14:57:14 +0500

    Created script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const newBtn = document.querySelector('.btn--new');
+const rollBtn = document.querySelector('.btn--roll');
+const holdBtn = document.querySelector('.btn--hold');
+const dice = document.querySelector('.dice');
+const player1 = document.querySelector('.player--0');
+const player2 = document.querySelector('.player--1');
+let currentPlayer = document.querySelector('.player--active');
+
+console.log(player1);
+console.log(player2);
+console.log(currentPlayer);
+
+let currentScore = 0;
+let totalScore = 0;
+let random;
