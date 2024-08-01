c40f46652c97501995ac4b7fe9fd4163212becdb
AW-2021 <aminawasif20@gmail.com>
2024-08-01 12:27:51 +0500

    Completed PROJECT #2

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,22 +6,27 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
+// Function to open modal window
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
+// Function to close modal window
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
+// Adding a 'click' event listener to each button
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
 
+// Event listeners to close modal window
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
+// Adding a 'keydown' event listener for Escape key
 document.addEventListener('keydown', function (e) {
   console.log(e.key);
 
