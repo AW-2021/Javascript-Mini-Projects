f045a0cab65b01c62feb24665b7c86ea6e10f25e
AW-2021 <aminawasif20@gmail.com>
2024-08-02 16:03:59 +0500

    Modified script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -5,21 +5,35 @@ function calcAge(birthYear) {
   // age has FUNCTION SCOPE
   const age = 2024 - birthYear;
 
-  // function printAge has FUNCTION/ BLOCK SCOPE
+  // function printAge has FUNCTION SCOPE
   function printAge() {
     // output has FUNCTION SCOPE
-    const output = `${firstName}, you are ${age}, born in ${birthYear}.`;
+    let output = `${firstName}, you are ${age}, born in ${birthYear}.`;
     console.log(output);
 
     if (birthYear >= 1991 && birthYear <= 1996) {
       // millenial has FUNCTION SCOPE
       var millenial = true;
+
+      // firstName will have BLOCK SCOPE
+      const firstName = 'John'; // John gets printed out in str, NOT Amy (closest scope lookup)
+
+      output = 'NEW OUTPUT!'; // Reassigning outer scope's output variable
+      // const output = 'NEW OUTPUT!'; // It will be completely new variable & cannot be printed outside block, as it has BLOCK SCOPE
+
       // str has BLOCK SCOPE
       const str = `Oh, and you're a millenial, ${firstName}!`;
       console.log(str);
+
+      // function add has BLOCK SCOPE (only in strict mode)
+      function add(a, b) {
+        return a + b;
+      }
     }
     // console.log(str); // -> str is not defined (Parent cannot access child variables)
     console.log(millenial);
+    // console.log(add(4, 5)); // -> add is not defined (function has block scope)
+    console.log(output); // -> NEW OUTPUT
   }
   printAge();
 
