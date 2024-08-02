5fe1e878b29f50a77b654b64dd07e5228f9490a7
AW-2021 <aminawasif20@gmail.com>
2024-08-02 10:38:43 +0500

    Changed reset() to an init function to script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -14,9 +14,23 @@ let currentScore = 0;
 let activePlayer = 0;
 let playing = true;
 
-// Hiding the dice at the start of the game
-diceEl.classList.add('hidden');
+const init = function () {
+  player1.querySelector('.score').textContent = 0;
+  player2.querySelector('.score').textContent = 0;
 
+  player1.querySelector('.current-score').textContent = 0;
+  player2.querySelector('.current-score').textContent = 0;
+
+  // Hiding the dice at the start of the game
+  diceEl.classList.remove('hidden');
+
+  player1.classList.remove('player--winner');
+  player2.classList.remove('player--winner');
+  player1.classList.add('player--active');
+  player2.classList.remove('player--active');
+};
+
+init();
 const switchPlayer = function () {
   currentScore = 0;
   document.getElementById(`current--${activePlayer}`).textContent =
@@ -70,7 +84,7 @@ const holdScore = function () {
       scores[activePlayer];
 
     // Checking if player's score is >= 100
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 20) {
       // Finish game
       playerWins();
     } else {
@@ -80,28 +94,7 @@ const holdScore = function () {
   }
 };
 
-const reset = function () {
-  currentScore = 0;
-  diceEl.classList.remove('hidden');
-
-  player1.querySelector('.current-score').textContent = 0;
-  player2.querySelector('.current-score').textContent = 0;
-
-  player1.querySelector('.score').textContent = 0;
-  player2.querySelector('.score').textContent = 0;
-
-  document
-    .querySelector(`.player--${activePlayer}`)
-    .classList.remove('player--winner');
-
-  currentPlayer = player1;
-  player1.classList.add('player--active');
-  player2.classList.remove('player--active');
-
-  diceEl.classList.remove('hidden');
-};
-
 // Roll Dice functionality
 rollBtn.addEventListener('click', rollDice);
 holdBtn.addEventListener('click', holdScore);
-newBtn.addEventListener('click', reset);
+newBtn.addEventListener('click', init);
