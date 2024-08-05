becb5a27689169d109ff4ac4537c717afcdac399
AW-2021 <aminawasif20@gmail.com>
2024-08-05 17:34:49 +0500

    Modified script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,46 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
+
+const arr = [2, 4, 6];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// DESTRUCTURING ARRAYS
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+let [primary, , secondary] = restaurant.categories;
+console.log(primary, secondary);
+
+// Swapping variables
+// const temp = primary;
+// primary = secondary;
+// secondary = temp;
+// console.log(primary, secondary);
+
+// Swapping Variables - OPTIMIZED
+[primary, secondary] = [secondary, primary];
+console.log(primary, secondary);
+
+// Receive two return values from a function
+const [starter, main] = restaurant.order(3, 1);
+console.log(starter, main);
+
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+
+const [i, , [j, k]] = nested; // Nested Destructuring
+console.log(i, j, k);
+
+// Default values
+const [p = 1, q = 1, r = 1] = [8];
+console.log(p, q, r);
