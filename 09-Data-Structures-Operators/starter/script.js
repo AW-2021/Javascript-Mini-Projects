8f93dae0ad618759af2e721b060060e52c3e9fd2
Amina Wasif <aminawasif20@gmail.com>
2024-08-21 17:05:20 +0500

    Extended script.js further

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -539,7 +539,8 @@ for (const [key, value] of question) {
     continue;
   }
 }
-const answer = prompt('Your answer: ');
+// const answer = prompt('Your answer: ');
+const answer = 3;
 console.log(answer);
 
 console.log(question.get(question.get('correct') === Number(answer)));
@@ -549,5 +550,186 @@ console.log([...question]);
 console.log([...question.keys()]);
 console.log([...question.values()]);
 console.log([...question.entries()]);
+
+/**********************************************************************************************************************/
+
+// STRINGS - PART 1
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('Portugal'));
+
+console.log(airline.slice(4)); // Does not change the underlying string, as strings are primitives
+console.log(airline);
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2)); // Includes last two letters
+console.log(airline.slice(1, -1)); // Excludes last letter
+
+const checkMiddleSeat = function (seat) {
+  // B and E are the middle seats
+  const s = seat.slice(-1);
+  console.log(
+    s === 'B' || s === 'E'
+      ? 'You got the middle seat 😬'
+      : "You didn't get a middle seat 🤩"
+  );
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('john'));
+console.log(typeof new String('john'));
+
+console.log(typeof new String('john').slice(1));
+
+/**********************************************************************************************************************/
+
+// STRINGS - PART 2
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fix capitalization in name
+const passenger = 'jOnAS';
+const passengerLower = passenger.toLowerCase();
+console.log(passengerLower);
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+// Check email
+const email = 'hello@jonas.io';
+const loginEmail = '  Hello@Jonas.Io \n';
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+
+const normalEmail = loginEmail.toLowerCase().trim();
+console.log(normalEmail);
+console.log(normalEmail === email);
+
+// Replacing in strings
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!';
+console.log(announcement.replace('door', 'gate'));
+console.log(announcement.replaceAll('door', 'gate'));
+
+// REGEX
+const regex = /door/gi;
+const announcementNew = announcement.replace(regex, 'gate');
+console.log(announcementNew);
+
+// Booleans
+const plane2 = 'Airbus A320neo';
+console.log(plane2.includes('A320'));
+console.log(plane2.includes('Boeing'));
+console.log(plane2.startsWith('Air'));
+
+if (plane2.startsWith('Airbus') && plane2.endsWith('neo')) {
+  console.log('Part of the NEW Airbus family!');
+}
+
+// Practice Exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  const regex = /knife|taser|gun|weapon/g;
+
+  if (regex.test(baggage)) {
+    console.log('NOT CLEAR! You are not allowed on board.');
+  } else {
+    console.log('Good to go! Welcome aboard!');
+  }
+
+  // if (baggage.includes('knife') || baggage.includes('taser')) {
+  //   console.log('NOT CLEAR! You are not allowed on board.');
+  // } else {
+  //   console.log('Good to go! Welcome aboard!');
+  // }
+};
+
+checkBaggage('I have a laptop, some Food and a pocket Knife.');
+checkBaggage('Just socks and a camera');
+checkBaggage('Got some snacks and an electric taser for protection');
+
+/**********************************************************************************************************************/
+
+// STRINGS - PART 3
+
+// split Method
+console.log('a+very+nice+string'.split('+'));
+console.log('Amelia Henderson'.split(' '));
+
+const [firstName, lastName] = 'Amelia Henderson'.split(' ');
+console.log(firstName, lastName);
+
+// join Method
+const fullName = ['Ms.', firstName, lastName.toUpperCase()].join(' ');
+console.log(fullName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  let namesUpper = [];
+
+  console.log(names);
+
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+
+  console.log(namesUpper);
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('jane doe');
+
+// padStart and padEnd Methods
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+')); // FORMAT: str.padStart(total length of str, padding character)
+console.log('John'.padStart(20, '/').padEnd(30, '/'));
+
+const maskCreditCard = function (number) {
+  const str = number.toString(); // ALT way to convert number to string: number + ''
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(6893321));
+console.log(maskCreditCard(37592785462));
+console.log(maskCreditCard(8302355611154747));
+
+// repeat Method
+const message2 = 'Bad weather... All departures delayed...';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'✈️'.repeat(n)}`);
+};
+
+planesInLine(3);
+planesInLine(6);
+planesInLine(12);
 
 /**********************************************************************************************************************/
