403e188c0700a9836ee66a2ca20c25a9dc791028
Amina Wasif <aminawasif20@gmail.com>
2024-08-23 15:34:41 +0500

    Extended script.js

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// DEFAULT PARAMETERS
+
 const bookings = [];
 
 const createBooking = function (
@@ -29,8 +31,11 @@ createBooking(
 
 /**********************************************************************************************************************/
 
-const flight = 'LH234';
+// HOW PASSING ARGUMENTS WORKS: VALUE VS. REFERENCE
+
+const flight = 'LH234'; // String : PRIMITIVE TYPE
 const amy = {
+  // Object : REFERENCE TYPE
   name: 'Amy Henderson',
   passport: 2945723091,
 };
@@ -49,3 +54,68 @@ const checkIn = function (flightNum, passenger) {
 checkIn(flight, amy);
 console.log(flight);
 console.log(amy);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(amy);
+checkIn(flight, amy);
+
+/**********************************************************************************************************************/
+
+// FUNCTIONS ACCEPTING CALLBACK FUNCTIONS
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  console.log(first, others);
+
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-Order Function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is pretty cool!', upperFirstWord);
+transformer('JavaScript is pretty cool!', oneWord);
+
+// JS uses callabcks all the time
+const high5 = function () {
+  console.log('👋');
+};
+
+// addEventListener is the higher-order function
+document.querySelector('h1').addEventListener('click', high5); // high5 is the callback function
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+/**********************************************************************************************************************/
+
+// FUNCTIONS RETURNING FUNCTIONS
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greetingHi = greet('Hi');
+greetingHi('John');
+greetingHi('Maria');
+
+greet('Hey')('Amy');
+
+// Challenge
+const greetArrow = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArrow('Hello')('Amy');
+
+/**********************************************************************************************************************/
