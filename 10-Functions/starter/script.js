b891c26e212ff2af8b42dcdfd31daadb075f044f
Amina Wasif <aminawasif20@gmail.com>
2024-08-27 13:01:28 +0500

    Extended script.js further

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -221,3 +221,94 @@ console.log(addVAT2(100));
 console.log(addVAT2(23));
 
 /**********************************************************************************************************************/
+
+// IMMEDIATELY INVOKED FUNCTION EXPRESSION (IIFE)
+
+const runOnce = function () {
+  console.log('This will run again');
+};
+runOnce();
+
+// IIFE
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})();
+
+// console.log(isPrivate);
+
+(() => console.log('This will ALSO never run again'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+// console.log(isPrivate);
+console.log(notPrivate);
+
+/**********************************************************************************************************************/
+
+// CLOSURES
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
+
+/**********************************************************************************************************************/
+
+// CLOSURE EXAMPLES
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassenger = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassenger(240, 3);
+
+/**********************************************************************************************************************/
