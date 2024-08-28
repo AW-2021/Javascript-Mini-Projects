1ff1a48692037651ce3c5b8dac9c6c72e3984c06
Amina Wasif <aminawasif20@gmail.com>
2024-08-28 17:33:29 +0500

    Extended script.js

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+// CONSTRUCTOR FUNCTIONS & THE NEW OPERATOR
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const john = new Person('John', 1991);
+console.log(john);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically returns {}
+
+const matilda = new Person('Matilda', 2017);
+const jill = new Person('Jill', 2001);
+console.log(matilda, jill);
+
+const jay = 'Jay';
+
+console.log(john instanceof Person); // -> true
+console.log(jay instanceof Person); // -> false
+
+/**********************************************************************************************************************/
+
+// PROTOTYPES
